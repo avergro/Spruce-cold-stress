c1525e9633f1c2367977ecc89d6a98ab6483e701
avergro <vergara.alexander@gmail.com>
2019-07-15 16:54:39 +0200

    Add files via upload

---
+++ Triming_promotors_lenghts.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl -w
+
+#This script take FASTA sequences as input in one row (.prepared file input) 
+
+my $file = shift @ARGV;
+
+open IN1, $file or die "Can't read $file: $!\n";
+
+while( my $seqs = <IN1> ){
+
+my @seqs = split(/\n/, $seqs);
+
+foreach $seq (@seqs) {
+
+if ($seq =~ /^>/) {         
+
+
+
+$seqend = substr($seq,0,10);
+chomp $seqend;
+print $seqend,"\n";
+} else {
+$seqend2 = substr($seq,-692);
+chomp $seqend2;
+print $seqend2,"\n";
+
+}
+}
+}
